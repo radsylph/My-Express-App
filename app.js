cff1d17a9f6a9974c0fcdc149a4c1676e2fc1015
Victor Kneider <victorkneider@gmail.com>
2023-02-09 14:43:00 -0400

    feat: added static files

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const session = require('express-session');
 const bd = require('./settings.js');
 const app = express();
-
+const path = require('path');
 
 
 app.use(cors());
 app.use(express.json());
 app.set('port', 3000)
+app.use(express.static('public'));
 app.use(session({
     secret: 'secret',
     resave:false,
@@ -20,74 +21,49 @@ app.use(session({
     }
 }));
 
-app.post('/smth', (req, res) => {
-    if(!req.session.logged){
-        res.send({error: "User not logged in"});
-    } else{
-        res.send({message: "User logged in"});
-    }
-});
-
-
-app.post('/login', (req, res) => {
-
-    if(req.body.user=="user" && req.body.password=="password"){
-        req.session.user = req.body.user;
-        req.session.logged=true;
-       // res.set('Set-Cookie', `sessionID=${req.sessionID}; HttpOnly;`)
-        res.send({message:"cookie granted"});
-    }else{
-        res.send({error: "User not found"});
-    }
-
-});
-
-app.post('/logout', (req, res) => {
-    req.session.destroy();
-    res.send({message:"cookie destroyed"});
-});
-
 app.get('/', (req, res) => {
-  res.send('Hello Worldx!.');
+    res.sendFile(path.join(__dirname,'/public/main/index.html'));
 });
+
 
 app.listen(app.get('port'), '', ()=>{
     console.log('Server is running on port: ' + app.get('port'));
 })
 
-app.post('/e', (req, res) => {
-    console.log(req.sessionID);
-    res.send({xd:2});
-});
 
-app.post('/users', (req, res) => {
-    if(users[findIndex(req.body.id)]==undefined){
-        res.send({error: "User not found"});   
+
+const users = new Map();
+
+app.post('/registerUser', (req, res) => {
+    users.set(req.body.email, req.body.password)
+    res.send({message: 'User added successfully'})
+    console.log("created")
+})
+
+app.post('/loginUser', (req, res) => {
+    
+    if(users.has(req.body.email)){
+        if(users.get(req.body.email) === req.body.password){
+            
+            req.session.logged=true;
+            res.send({message:true})
+            
+        }else{
+            console.log('2')
+            res.send({message: 'Wrong password'})
+        } 
+    } else {
+        res.send({message: 'User not found'})
+    }
+
+})
+
+app.get('/home', (req, res) => {
+
+    if(req.session.logged){ 
+        res.sendFile(path.join(__dirname,'/public/Logged/logged.html'));
     }else{
-      res.send(users[findIndex(req.body.id)]);
+        //redirecciona pa fuera
     }
-});
 
-
-
-const users = [
-    {
-        id: 1,
-        name: "John",
-        age: 30,
-        city: "New York"
-    },
-    {
-        id: 2,
-        name: "Jane",
-        age: 25,
-        city: "London"
-    },
-    {
-        id: 3,
-        name: "Bob",
-        age: 40,
-        city: "Paris"
-    }
-]
-
+})
